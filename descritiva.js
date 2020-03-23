8dc023d7999b9baf36c0fd2e594f6f42f350cd96
André Pedro Gomes <andrepedro51@gmail.com>
2020-03-23 14:49:05 -0300

    Descritiva adicionada

---
+++ descritiva.js
@@ -0,0 +1,45 @@
+let medida = document.getElementById("medida")
+let barra = document.getElementById("barraMedida")
+let vlrBarra = document.getElementById('vlrBarra')
+
+function valorBarra(){
+    vlrBarra.innerHTML = barra.value + "%"
+    barra.oninput = function(){
+        vlrBarra.innerHTML = barra.value + "%"
+    }
+}
+
+function Separatriz(){
+    
+    if (medida.value == "Quartil"){
+        barra.step = 25
+        barra.max = 100
+        barra.value = 0
+        vlrBarra.innerHTML = barra.value + "%"
+    } else if (medida.value == 'Quintil'){
+        barra.step = 20
+        barra.max = 100
+        barra.value = 0
+        vlrBarra.innerHTML = barra.value + "%"
+    } else if (medida.value == 'Decil'){
+        barra.step = 10
+        barra.max = 100
+        barra.value = 0
+        vlrBarra.innerHTML = barra.value + "%"
+    } else if (medida.value == 'Percentil'){
+        barra.step = 1
+        barra.max = 100
+        barra.value = 0
+        vlrBarra.innerHTML = barra.value + "%"
+    } else {
+        barra.step = 1
+        barra.max = 0
+        barra.value = 0
+        vlrBarra.innerHTML = barra.value + "%"
+    }
+}
+function Calcular(){
+    
+}
+
+
